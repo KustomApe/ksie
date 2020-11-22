af5f8d73200e2efa77058d7b2ef8da41d94f3017
inke.KustomApe <kustomape@gmail.com>
2020-11-22 15:00:37 +0900

    Updated uranai.js.

--- assets/js/omikuji.js
+++ assets/js/omikuji.js
@@ -1,12 +1,12 @@
-function omikuji() {
-    pokemon = new Array('ポケモン1', 'ポケモン2', 'ポケモン3', 'ポケモン4', 'ポケモン5');
+function uranai() {
     rand = Math.floor(Math.random()*100);
     console.log(rand)
-    elect = 'ポケモン1'
-    if (rand > 9) elect = "ポケモン2"; //10～29（20%）
-	if (rand > 29) elect = "ポケモン3"; //30～69（40%）
-	if (rand > 69) elect = "ポケモン4"; //70～89（20%）
-	if (rand > 89) elect = "ポケモン5"; //90～99（10%）
+    elect = 'サンダー'
+    if (rand > 9) elect = "ランドロス"; //10～29（20%）
+    if (rand > 29) elect = "カプ・レヒレ"; //30～49（20%）
+    if (rand > 49) elect = "ボルトロス"; //50～69(20%)
+	if (rand > 69) elect = "ミミッキュ"; //70～89（20%）
+	if (rand > 89) elect = "エースバーン"; //90～99（10%）
     console.log(elect);
     para = document.getElementById('pokemon_elect');
     para.innerHTML = elect;
