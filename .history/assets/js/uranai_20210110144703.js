a4ca881e754596c117450ac469f050841a98ecdc
Eisuke Nakanishi <kustomape@gmail.com>
2021-01-10 15:36:34 +0900

    Adds several changes.

---
+++ .history/assets/js/uranai_20210110144703.js
@@ -0,0 +1,19 @@
+function uranai() {
+    rand = Math.floor(Math.random()*100);
+    console.log(rand)
+    elect = 'サンダー'
+    if (rand > 9) elect = "ランドロス"; //10～29（20%）
+    if (rand > 29) elect = "カプ・レヒレ"; //30～49（20%）
+    if (rand > 49) elect = "ボルトロス"; //50～69(20%)
+	if (rand > 69) elect = "ミミッキュ"; //70～89（20%）
+	if (rand > 89) elect = "エースバーン"; //90～99（10%）
+    console.log(elect);
+    para = document.getElementById('pokemon_elect');
+    para.innerHTML = elect;
+}
+
+function taipu() {
+    // タイプのデータを取ってくる
+    // タイプの中身をコンソールログに出す
+    // 
+}
